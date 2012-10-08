3a6bbf22bc4cfeab615c1d391bae7c1363d9d1aa
Biohazard1986@gmail.com <Biohazard1986@gmail.com@e0bd7100-9e06-674f-8c31-83d2b76536c5>
2012-10-08 22:56:04 +0000

    Not sure, commit from new Eclipse version.

---
+++ trunk/ServletMiscFilters/src/es/biohazardwtf/miscfilters/MaintenanceFilter.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package es.biohazardwtf.miscfilters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author Guille
+ *
+ */
+public class MaintenanceFilter implements Filter {
+
+	//IDEA: http://tutorials.jenkov.com/java-web-apps/web-app-maintenance.html
+	
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}	
+
+}
